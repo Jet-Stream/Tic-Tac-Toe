c1074bf39ea38f95b0a6cc2718ea3470f2b4c462
Tanner <tjohnsto8@gmail.com>
2020-10-28 14:13:40 -0400

    Logic for taking turns and determining a winner or a draw implemented. Needs a play again function and refactoring into classes

--- ttt.rb
+++ ttt.rb
@@ -4,10 +4,9 @@ row_3 = [" ", " ", " "]
 p row_1
 p row_2
 p row_3
-#player 1's turn: inputs column and row, board updates with X
-#create active_player variable to update which symbol is added to board
-#create loop to cycle through turns while victory condition is false
-
+#On active player's turn, program asks for column and row, board updates with active player's symbol
+#Active_player variable determines which symbol is added to board
+#Game loop to cycles through turns while victory condition is false or until the board is full
 
 victory = false
 active_player_counter = 1
@@ -18,51 +17,171 @@ if active_player_counter % 2 == 0
 	active_player_symbol = "X"
 	end
 active_player_counter += 1
-puts "Choose a row (top, middle, or bottom):"
-row_selection = gets.chomp
-puts "Choose a column(left, middle, or right):"
-column_selection = gets.chomp
+puts "#{active_player_symbol}, Choose a row (top, middle, or bottom):"
+row_selection = gets.downcase.chomp
+until row_selection == "top" || row_selection == "middle" || row_selection == "bottom" do
+	puts "Sorry, didn't catch that. Try again"
+	row_selection = gets.downcase.chomp
+	end
+puts "#{active_player_symbol}, Choose a column(left, middle, or right):"
+column_selection = gets.downcase.chomp
+until column_selection == "left" || column_selection == "middle" || column_selection == "right" do
+	puts "Sorry, didn't catch that. Try again"
+	column_selection = gets.downcase.chomp
+	end
+
+#board placements
 if row_selection == "middle" && column_selection == "middle"
-	row_2[1] = active_player_symbol
-	end	
+	if row_2[1] == " "
+		row_2[1] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1 #to keep the active player from changing
+		end
+	end
 if row_selection == "top" && column_selection == "left"
-	row_1[0] = active_player_symbol
+	if row_1[0] == " "
+		row_1[0] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 if row_selection == "top" && column_selection == "middle"
-	row_1[1] = active_player_symbol
+	if row_1[1] == " "
+		row_1[1] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 if row_selection == "top" && column_selection == "right"
-	row_1[2] = active_player_symbol
+	if row_1[2] == " "
+		row_1[2] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 if row_selection == "middle" && column_selection == "left"
-	row_2[0] = active_player_symbol
+	if row_2[0] == " "
+		row_2[0] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 if row_selection == "middle" && column_selection == "right"
-row_2[2] = active_player_symbol
+	if row_2[2] == " "
+		row_2[2] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
+	end
 end
 if row_selection == "bottom" && column_selection == "left"
-	row_3[0] = active_player_symbol
+	if row_3[0] == " "
+	 row_3[0] = active_player_symbol
+ 	else
+ 		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 if row_selection == "bottom" && column_selection == "middle"
+	if row_3[1] == " "
 	row_3[1] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 if row_selection == "bottom" && column_selection == "right"
+	if row_3[2] == " "
 	row_3[2] = active_player_symbol
+	else
+		puts "That space has already been taken"
+		active_player_counter -= 1
 	end
+end
 p row_1
 p row_2
 p row_3
+
+#win conditions
+#row wins
 if row_1[0] == "X" && row_1[1] == "X" && row_1[2] == "X"
 	victory = true
 	puts "X wins!"
 	end
 if row_1[0] == "O" && row_1[1] == "O" && row_1[2] == "O"
-	victory = true
-	puts "O wins!"
+victory = true
+puts "O wins!"
 	end
+if row_2[0] == "X" && row_2[1] == "X" && row_2[2] == "X"
+victory = true
+puts "X wins!"
+end
+if row_2[0] == "O" && row_2[1] == "O" && row_2[2] == "O"
+victory = true
+puts "O wins!"
+end
+if row_3[0] == "X" && row_3[1] == "X" && row_3[2] == "X"
+victory = true
+puts "X wins!"
+end
+if row_3[0] == "O" && row_3[1] == "O" && row_3[2] == "O"
+victory = true
+puts "O wins!"
+end
+	
+#diagonal wins
+if row_1[0] == "X" && row_2[1] =="X" && row_3[2] =="X"
+	victory = true
+	puts "X wins!"
+	end
+if row_1[0] == "O" && row_2[1] =="O" && row_3[2] =="O"
+victory = true
+puts "O wins!"
+end
+if row_1[2] == "X" && row_2[1] =="X" && row_3[0] =="X"
+	victory = true
+	puts "X wins!"
+	end
+if row_1[2] == "O" && row_2[1] =="O" && row_3[0] =="O"
+victory = true
+puts "O wins!"
+end
+
+#column wins
+if row_1[0] == "X" && row_2[0] == "X" && row_3[0] == "X"
+	victory = true
+	puts "X wins!"
+	end
+if row_1[0] == "O" && row_2[0] == "O" && row_3[0] == "O"
+victory = true
+puts "O wins!"
+	end
+if row_1[1] == "X" && row_2[1] == "X" && row_3[1] == "X"
+victory = true
+puts "X wins!"
+end
+if row_1[1] == "O" && row_2[1] == "O" && row_3[1] == "O"
+victory = true
+puts "O wins!"
+end
+if row_1[2] == "X" && row_2[2] == "X" && row_3[2] == "X"
+victory = true
+puts "X wins!"
+end
+if row_1[2] == "O" && row_2[2] == "O" && row_3[2] == "O"
+victory = true
+puts "O wins!"
+end
+
+#draw condition
 if row_1.all? {|cell| cell != " "} == true && row_2.all? {|cell| cell != " "} == true && row_3.all? {|cell| cell != " "} == true && victory == false
 puts "It's a draw!"
+break
 end
 
 end
-
